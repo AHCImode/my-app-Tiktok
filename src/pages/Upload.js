d530cc0246cc3076cdf0328f1d27bb55ef111448
AHCImode <78480842+AHCImode@users.noreply.github.com>
2021-03-22 23:57:00 +0300

    p

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,7 +1,8 @@
 const Upload = () =>  {
     return (
         <div>
-            <h1>Upload video</h1>       
+            <h1>Upload video</h1>     
+            <p>Helloc </p>
         </div>
     )
 }
